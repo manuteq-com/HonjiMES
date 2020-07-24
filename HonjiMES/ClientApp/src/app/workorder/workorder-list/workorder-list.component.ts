e66652d0bb6d9e134c17bfe38e082931fe208db7
黃浩維 <pigmay26@gmail.com>
2020-07-24 13:59:38 +0800

    test

--- HonjiMES/ClientApp/src/app/workorder/workorder-list/workorder-list.component.ts
+++ HonjiMES/ClientApp/src/app/workorder/workorder-list/workorder-list.component.ts
@@ -40,6 +40,7 @@ export class WorkorderListComponent implements OnInit {
         this.mod = 'report';
     }
     tdclick(e) {
+        debugger;
         // notify({
         //     message: e.ProductNo,
         //     position: {
