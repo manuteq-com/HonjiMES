fe1ade5d53c0bdd5ac2a852bdc2d1713e88927ea
minjia <sofie59203@gmail.com>
2020-12-16 17:35:05 +0800

    修改機台管理資料排序方式

--- HonjiMES/Controllers/MachineManagementController.cs
+++ HonjiMES/Controllers/MachineManagementController.cs
@@ -42,7 +42,8 @@ namespace HonjiMES.Controllers
         {
             _context.ChangeTracker.LazyLoadingEnabled = true;
             var WorkOrderDetails = await _context.WorkOrderDetails.Where(x => x.DeleteFlag == 0).ToListAsync();
-            var machine = _context.WorkOrderDetails.AsEnumerable().Where(y => y.DeleteFlag == 0 && !string.IsNullOrWhiteSpace(y.ProducingMachine)&& (y.Status == 1 || y.Status == 2)).GroupBy(x => x.ProducingMachine).OrderBy(x => x.Key).ToList();
+            var machine = _context.WorkOrderDetails.AsEnumerable().Where(y => y.DeleteFlag == 0 && !string.IsNullOrWhiteSpace(y.ProducingMachine)&& (y.Status == 1 || y.Status == 2))
+            .OrderByDescending(x => x.ActualStartTime).GroupBy(x => x.ProducingMachine).OrderBy(x => x.Key).ToList();
             
             // 工單Head為[已派工]
             var dataAssign = WorkOrderDetails.Where(x => x.DeleteFlag == 0 && x.Status == 1); 
