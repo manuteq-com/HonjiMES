f5459d80300f095dc59c95cde251ae7345431b1e
hsin <hsin@manuteq.com>
2021-01-05 11:04:33 +0800

    [修正]app.module缺少逗號

--- HonjiMES/ClientApp/src/app/app.module.ts
+++ HonjiMES/ClientApp/src/app/app.module.ts
@@ -298,7 +298,7 @@ export function tokenGetter() {
         CreateBomComponent,
         MachineorderReportComponent,
         ChatHubComponent,
-        ToolmanagementComponent
+        ToolmanagementComponent,
         MachineorderBoardComponent
     ],
     providers: [
