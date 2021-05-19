f1c9e0ea58cfd836d57d9a1d516ea0e76b750f1e
ben <ben@manuteq.com>
2021-05-19 14:36:45 +0800

    Revert "rwd devextreme基本框架"
    This reverts commit 81834c76c7cd90c39a2bffa5956b6ae50906f967.
    
    # Conflicts:
    #	HonjiMES/ClientApp/src/app/app-navigation.ts
    #	HonjiMES/ClientApp/src/app/app.component.html

--- HonjiMES/ClientApp/src/app/service/auth.service.ts
+++ HonjiMES/ClientApp/src/app/service/auth.service.ts
@@ -4,12 +4,13 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { LoginUser } from '../model/loginuser';
 import { APIResponse } from '../app.module';
-import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
     public currentUserSubject: BehaviorSubject<LoginUser>;
     public currentUser: Observable<LoginUser>;
+
     constructor(private http: HttpClient) {
         this.currentUserSubject = new BehaviorSubject<LoginUser>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
