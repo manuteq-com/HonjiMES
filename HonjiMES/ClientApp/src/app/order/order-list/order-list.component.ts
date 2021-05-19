e9e043b46342c32c27fb6f97dee47145af5339b2
ben <ben@manuteq.com>
2021-05-19 17:01:17 +0800

    Revert "採購單,客戶訂單 統一修訂排版"
    This reverts commit 6231856a7b588008561265baa855c96e9526a509.
    
    # Conflicts:
    #	HonjiMES/ClientApp/src/app/order/creatorder/creatorder.component.html

--- HonjiMES/ClientApp/src/app/order/order-list/order-list.component.ts
+++ HonjiMES/ClientApp/src/app/order/order-list/order-list.component.ts
@@ -435,4 +435,10 @@ export class OrderListComponent {
         this.dataGrid.instance.refresh();
     }
 
+    onContentReady(e) {
+        var contentElement = e.component.content();
+        contentElement.style.padding = '0px';
+        contentElement.style.margin = '5px';
+      }
+
 }
