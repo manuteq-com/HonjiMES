abdebaafc9b0a519ff64cc1f4e7f8b8966669529
hsin0724 <67618752+hsin0724@users.noreply.github.com>
2021-06-11 15:09:28 +0800

    修正新增領料單頁面選擇工單號的部分僅能顯示幾筆資料

--- HonjiMES/ClientApp/src/app/receive/creatreceive/creatreceive.component.ts
+++ HonjiMES/ClientApp/src/app/receive/creatreceive/creatreceive.component.ts
@@ -197,7 +197,7 @@ export class CreatreceiveComponent implements OnInit, OnChanges {
             dataSource: new CustomStore({
                 key: 'Id',
                 load: (loadOptions) => {
-                    loadOptions.take = 20;
+                    // loadOptions.take = 20;
                     loadOptions.sort = [{ selector: 'WorkOrderNo', desc: true }];
                     if (loadOptions.searchValue) {
                         loadOptions.filter = [loadOptions.searchExpr, loadOptions.searchOperation, loadOptions.searchValue];
