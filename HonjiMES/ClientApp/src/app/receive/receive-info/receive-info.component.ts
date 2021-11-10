dc47f5d952ca9c29f1adc1668d21e0dfd8c5ca76
ben <ben@manuteq.com>
2021-11-10 16:33:49 +0800

    領料資料>紀錄查詢， 加入品號查詢

--- HonjiMES/ClientApp/src/app/receive/receive-info/receive-info.component.ts
+++ HonjiMES/ClientApp/src/app/receive/receive-info/receive-info.component.ts
@@ -39,6 +39,8 @@ export class ReceiveInfoComponent implements OnInit, OnChanges {
     dataSourceDB1: any;
     dataSourceDB2: any;
     Warehouselist: CustomStore;
+    MaterialBasicList: any;
+    MaterialBasicSelectBoxOptions: any;
     constructor(private http: HttpClient, public app: AppComponent) {
         this.formData = {};
         // this.editOnkeyPress = true;
@@ -47,9 +49,30 @@ export class ReceiveInfoComponent implements OnInit, OnChanges {
         this.labelLocation = 'left';
         this.readOnly = false;
         this.showColon = true;
-        this.minColWidth = 100;
-        this.colCount = 1;
+        this.minColWidth = 300;
+        this.colCount = 2;
         this.NumberBoxOptions = { showSpinButtons: true, mode: 'number', min: 0, value: 0 };
+
+        //this.MaterialBasicList = SendService.sendRequest(this.http, '/Materials/GetMaterialBasics');
+        this.app.GetData('/MaterialBasics/GetMaterialBasicsAsc').subscribe(
+            (s) => {
+                if (s.success) {
+                    this.MaterialBasicList = s.data;
+                    // this.MaterialBasicList.forEach(x => {
+                    //     x.Name = x.MaterialNo + '_' + x.Name;
+                    // });
+                    this.MaterialBasicSelectBoxOptions = {
+                        dataSource: { paginate: true, store: { type: 'array', data: this.MaterialBasicList, key: 'Id' } },
+                        searchEnabled: true,
+                        displayExpr: 'MaterialNo',
+                        valueExpr: 'Id',
+                        onValueChanged: this.onValueChangedDataNo.bind(this),
+                    };
+
+                }
+            }
+        );
+
         this.editorOptions = {
             onValueChanged: this.onValueChanged.bind(this),
             dataSource: new CustomStore({
@@ -77,6 +100,7 @@ export class ReceiveInfoComponent implements OnInit, OnChanges {
 
     }
     ngOnChanges() {
+        console.log("afff");
         this.formData = {};
         if (this.itemkeyval !== null) {
             this.formData.WorkOrderNo = this.itemkeyval;
@@ -113,11 +137,48 @@ export class ReceiveInfoComponent implements OnInit, OnChanges {
                         }
                     );
                 }
+               if(this.formData.WorkOrderNo){
+                  this.formData.MaterialBasicId = undefined;
+               }
             }
         );
     }
     async onValueChanged(e) {
-        this.buttondisabled = false;
+        // this.buttondisabled = false;
         this.GetRequisitionsDetail(e.value);
+
+    }
+
+    async onValueChangedDataNo(e) {
+        this.formData.WorkOrderNo = undefined;
+        let key = e.value;
+        this.app.GetData('/Users/GetUsers').subscribe(
+            (s2) => {
+                if (s2.success) {
+                    this.app.GetData(this.Controller + '/GetRequisitionsDetailMaterialByProductBasicId/' + key).subscribe(
+                        (s) => {
+                            if (s.success) {
+                                this.dataSourceDB1 = s.data;
+                            }
+                        }
+                    );
+                    this.app.GetData(this.Controller + '/GetRequisitionsDetailByProductBasicId/' + key).subscribe(
+                        (s) => {
+                            if (s.success) {
+                                s.data.forEach(element => {
+                                    element.CreateUser = s2.data.find(x => x.Id === element.CreateUser).Realname;
+                                    if (element.ReceiveUser) {
+                                        element.ReceiveUser = s2.data.find(x => x.Id === element.ReceiveUser).Realname;
+                                    }
+                                    element.ReceiveQty = element.ReceiveQty === 0 ? null : element.ReceiveQty;
+                                    element.RbackQty = element.RbackQty === 0 ? null : element.RbackQty;
+                                });
+                                this.dataSourceDB2 = s.data;
+                            }
+                        }
+                    );
+                }
+            }
+        );
     }
 }
